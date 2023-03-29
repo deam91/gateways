160c4fc0fc12a677b97578abc12e8031289bbbce
ae-deam <deam@aleph.engineering>
2023-03-29 14:12:00 -0400

    Refactor app component

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,35 +1,169 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {fakeAsync, TestBed, tick} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
+import {GatewayService} from "./common/services/gateway.service";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {MatDialog, MatDialogConfig, MatDialogModule} from "@angular/material/dialog";
+import {MaterialModule} from "./common/shared/material.module";
+import {expect} from "@angular/flex-layout/_private-utils/testing";
+import {of} from "rxjs";
+import {CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA} from "@angular/core";
+import {ManageGatewayComponent} from "./components/manage-gateway/manage-gateway.component";
+import {GatewayModel} from "./common/models/gateway.model";
+import {ManageDeviceComponent} from "./components/manage-device/manage-device.component";
 
 describe('AppComponent', () => {
+  let app: AppComponent;
+  let service: GatewayService;
+  let modalServiceSpy: jasmine.SpyObj<MatDialog>;
+  let dialogRefSpyObj = jasmine.createSpyObj(
+    {
+      afterClosed: of({}),
+      close: null,
+      componentInstance: {
+        onAdd: (data: any) => of({data})
+      }
+    }
+  );
+
   beforeEach(async () => {
+    modalServiceSpy = jasmine.createSpyObj('modalService', ['open']);
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientTestingModule,
+        MaterialModule,
+        MatDialogModule,
       ],
       declarations: [
         AppComponent
       ],
+      providers: [
+        GatewayService,
+        {provide: MatDialog, useValue: modalServiceSpy}
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
     }).compileComponents();
+
+    modalServiceSpy.open.and.returnValue(dialogRefSpyObj);
+
+    const fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+    service = TestBed.inject(GatewayService);
+    fixture.detectChanges();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'gateways'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app.title).toEqual('gateways');
+    expect(app.gateways).toEqual([]);
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('gateways app is running!');
+  it('should set the breakpoint', () => {
+    let spy = spyOn(app, 'getInnerWidth').and.returnValue(400);
+    spyOn(app, 'setBreakpoint').and.callThrough();
+
+    app.setBreakpoint();
+    expect(app.breakpoint).toEqual(1);
+    expect(app.getInnerWidth).toHaveBeenCalled();
+    expect(app.setBreakpoint).toHaveBeenCalled();
+
+    spy.and.returnValue(600);
+    app.setBreakpoint();
+    expect(app.breakpoint).toEqual(2);
+    expect(app.getInnerWidth).toHaveBeenCalled();
+    expect(app.setBreakpoint).toHaveBeenCalled();
+
+    spy.and.returnValue(800);
+    app.setBreakpoint();
+    expect(app.breakpoint).toEqual(3);
+    expect(app.getInnerWidth).toHaveBeenCalled();
+    expect(app.setBreakpoint).toHaveBeenCalled();
+
+    spy.and.returnValue(1000);
+    app.setBreakpoint();
+    expect(app.breakpoint).toEqual(4);
+    expect(app.getInnerWidth).toHaveBeenCalled();
+    expect(app.setBreakpoint).toHaveBeenCalled();
+
+    spy.and.returnValue(1300);
+    app.setBreakpoint();
+    expect(app.breakpoint).toEqual(5);
+    expect(app.getInnerWidth).toHaveBeenCalled();
+    expect(app.setBreakpoint).toHaveBeenCalled();
   });
+
+  it('should change breakpoint on resize', () => {
+    let spy = spyOn(app, 'getInnerWidth').and.returnValue(400);
+    spyOn(app, 'setBreakpoint').and.callThrough();
+
+    app.onResize({});
+    expect(app.breakpoint).toEqual(1);
+    expect(app.getInnerWidth).toHaveBeenCalled();
+    expect(app.setBreakpoint).toHaveBeenCalled();
+
+    spy.and.returnValue(600);
+    app.onResize({});
+    expect(app.breakpoint).toEqual(2);
+    expect(app.getInnerWidth).toHaveBeenCalled();
+    expect(app.setBreakpoint).toHaveBeenCalled();
+  });
+
+  it(`should get gateways`, fakeAsync(() => {
+    spyOn(app, 'getGateways').and.callThrough();
+    spyOn(service, 'getGateways').and.callFake(() => of([]));
+
+    app.getGateways();
+    tick(1000);
+    expect(app.getGateways).toHaveBeenCalled();
+    expect(service.getGateways).toHaveBeenCalled();
+  }));
+
+  it('should open add gateway modal', () => {
+    const fakeDialogConfig = new MatDialogConfig;
+    fakeDialogConfig.panelClass = 'gateway-dialog';
+    fakeDialogConfig.maxHeight = '80%';
+    fakeDialogConfig.maxWidth = '80%';
+    fakeDialogConfig.minWidth = '400px';
+    fakeDialogConfig.minHeight = '50%';
+    fakeDialogConfig.disableClose = true;
+    app.addGatewayModal();
+
+    expect(modalServiceSpy.open).toHaveBeenCalledWith(ManageGatewayComponent, fakeDialogConfig);
+    expect(dialogRefSpyObj.afterClosed).toHaveBeenCalled();
+  })
+
+  it('should open edit gateway modal', () => {
+    let gateway = new GatewayModel('serial', 'name', '12.32.12.43', []);
+    const fakeDialogConfig = new MatDialogConfig;
+    fakeDialogConfig.panelClass = 'gateway-dialog';
+    fakeDialogConfig.maxHeight = '80%';
+    fakeDialogConfig.maxWidth = '80%';
+    fakeDialogConfig.minWidth = '400px';
+    fakeDialogConfig.minHeight = '50%';
+    fakeDialogConfig.disableClose = true;
+    fakeDialogConfig.data = gateway;
+    app.editGatewayModal(gateway);
+
+    expect(modalServiceSpy.open).toHaveBeenCalledWith(ManageGatewayComponent, fakeDialogConfig);
+    expect(dialogRefSpyObj.afterClosed).toHaveBeenCalled();
+  })
+
+  it('should open gateway devices modal', () => {
+    let gateway = new GatewayModel('serial', 'name', '12.32.12.43', []);
+
+    let dialogConfig = new MatDialogConfig;
+    dialogConfig.panelClass = 'gateway-devices-dialog';
+    dialogConfig.maxHeight = '80%';
+    dialogConfig.maxWidth = '80%';
+    dialogConfig.minWidth = '400px';
+    dialogConfig.minHeight = '50%';
+    dialogConfig.disableClose = true;
+    dialogConfig.data = {devices: gateway.devices, gateway: gateway};
+    app.openGatewayDevices(gateway);
+
+    expect(modalServiceSpy.open).toHaveBeenCalledWith(ManageDeviceComponent, dialogConfig);
+    expect(dialogRefSpyObj.afterClosed).toHaveBeenCalled();
+  })
+
 });
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {GatewayService} from "./common/services/gateway.service";
+import {MatDialog, MatDialogConfig} from "@angular/material/dialog";
+import {GatewayModel} from "./common/models/gateway.model";
+import {ManageGatewayComponent} from "./components/manage-gateway/manage-gateway.component";
+import {ManageDeviceComponent} from "./components/manage-device/manage-device.component";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,91 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gateways';
+  breakpoint: number = 3;
+  gateways: GatewayModel[] = [];
+  gatewaysObservable$: Observable<GatewayModel[]> | undefined;
+
+  constructor(private gatewayService: GatewayService,
+              private matDialog: MatDialog) {
+  }
+
+  addGatewayModal(): void {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.panelClass = 'gateway-dialog';
+    dialogConfig.maxHeight = '80%';
+    dialogConfig.maxWidth = '80%';
+    dialogConfig.minWidth = '400px';
+    dialogConfig.minHeight = '50%';
+    dialogConfig.disableClose = true;
+
+    const dialog = this.matDialog.open(ManageGatewayComponent, dialogConfig);
+    dialog.afterClosed().subscribe((data) => {
+      if (data) {
+        this.gatewaysObservable$ = this.getGateways();
+      }
+    });
+  }
+
+  editGatewayModal(gateway: GatewayModel): void {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.panelClass = 'gateway-dialog';
+    dialogConfig.maxHeight = '80%';
+    dialogConfig.maxWidth = '80%';
+    dialogConfig.minWidth = '400px';
+
+    dialogConfig.minHeight = '50%';
+    dialogConfig.disableClose = true;
+    dialogConfig.data = gateway;
+
+    const dialog = this.matDialog.open(ManageGatewayComponent, dialogConfig);
+    dialog.afterClosed().subscribe((data) => {
+      if (data) {
+        this.gatewaysObservable$ = this.getGateways();
+      }
+    });
+  }
+
+  setBreakpoint(): void {
+    let width = this.getInnerWidth();
+    if (width <= 450) this.breakpoint = 1;
+    else if (width <= 700) this.breakpoint = 2;
+    else if (width <= 900) this.breakpoint = 3;
+    else if (width <= 1200) this.breakpoint = 4;
+    else this.breakpoint = 5;
+  }
+
+  getInnerWidth(): number {
+    return window.innerWidth;
+  }
+
+  ngOnInit() {
+    this.setBreakpoint();
+    this.gatewaysObservable$ = this.getGateways();
+  }
+
+  onResize(event: any): void {
+    this.setBreakpoint();
+  }
+
+  getGateways(): Observable<GatewayModel[]> {
+    return this.gatewayService.getGateways();
+  }
+
+  openGatewayDevices(gateway: GatewayModel) {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.panelClass = 'gateway-devices-dialog';
+    dialogConfig.maxHeight = '80%';
+    dialogConfig.maxWidth = '80%';
+    dialogConfig.minWidth = '400px';
+
+    dialogConfig.minHeight = '50%';
+    dialogConfig.disableClose = true;
+    dialogConfig.data = {devices: gateway.devices, gateway: gateway};
+    const dialog = this.matDialog.open(ManageDeviceComponent, dialogConfig);
+    dialog.afterClosed().subscribe((data) => {
+      if (data) {
+        this.gatewaysObservable$ = this.getGateways();
+      }
+    });
+  }
 }
