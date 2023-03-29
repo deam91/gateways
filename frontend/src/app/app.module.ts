7c8c7290943c7453ed92a9da123a398272e4d715
ae-deam <deam@aleph.engineering>
2023-03-29 14:12:10 -0400

    Refactor app module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,22 +4,45 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {MaterialModule} from "./common/shared/material.module";
+import {GatewayService} from "./common/services/gateway.service";
+import {APIInterceptor} from "./common/interceptors/api.interceptor";
+import {ManageGatewayComponent} from "./components/manage-gateway/manage-gateway.component";
+import {ManageDeviceComponent} from "./components/manage-device/manage-device.component";
+import {ReactiveFormsModule} from "@angular/forms";
+import {FlexLayoutModule} from "@angular/flex-layout";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ManageGatewayComponent,
+    ManageDeviceComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
     AppRoutingModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: !isDevMode(),
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }),
+    MaterialModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: APIInterceptor,
+      multi: true,
+    },
+    GatewayService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
